26446cd96d4fd7c9c0d6c2dfefc6abd8c0591f4f
Jiri Danecek <danecek@fit.cvut.cz>
2017-10-16 14:16:04 +0200

    Merge origin/master
    Conflicts:
    	richclient/pom.xml

--- utils/src/main/java/lib2017/utils/Activator.java
+++ utils/src/main/java/lib2017/utils/Activator.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package lib2017;
+package lib2017.utils;
 
 import java.util.logging.Logger;
 import org.osgi.framework.BundleActivator;
@@ -12,12 +12,14 @@ import org.osgi.framework.BundleContext;
 
 public class Activator implements BundleActivator {
 
+    @Override
     public void start(BundleContext bc) throws Exception {
-        LOG.info("");
+        LOG.info(Messages.Book.getMessage());
        // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     private static final Logger LOG = Logger.getLogger(Activator.class.getName());
 
+    @Override
     public void stop(BundleContext bc) throws Exception {
 //        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
